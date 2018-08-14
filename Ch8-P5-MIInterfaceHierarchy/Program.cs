bcc47413a2869dc3ce4c5c5e26c708d873978f9d
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-08-14 13:06:19 +0500

    first commit from park

---
+++ Ch8-P5-MIInterfaceHierarchy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8_P5_MIInterfaceHierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
